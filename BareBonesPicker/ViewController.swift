8449b53a1129b682083db5644c882f8c0ae96e41
Jason Gresh <jrgresh@gmail.com>
2017-02-02 13:15:57 -0500

    Images working

---
+++ BareBonesPicker/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BareBonesPicker
+//
+//  Created by Jason Gresh on 2/2/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func pickImage(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
